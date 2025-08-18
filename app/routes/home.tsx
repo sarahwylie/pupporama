84f937d8d0d309ae99555e40c4088bc6276c7112
Sarah Wylie <sarahannemariawylie@gmail.com>
2025-08-18 12:28:28 -0500

    simple site

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Puppies" },
+    { name: "description", content: "Meet our Puppies!" },
   ];
 }
 
