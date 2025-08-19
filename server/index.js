7a98454027e3377cf5a6035f070bb2f097283a79
Sarah Wylie <sarahannemariawylie@gmail.com>
2025-08-18 20:35:38 -0500

    Updates

--- server/index.js
+++ server/index.js
@@ -102,20 +102,20 @@ const route0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProper
   default: root,
   links
 }, Symbol.toStringTag, { value: "Module" }));
-const DNA = "/assets/WillaDNA-D9B0LrEA.PNG";
-const WillaWoo = "/assets/WillaFace-DZ8op2ox.jpg";
-const Preggers = "/assets/Preggers-1_efbYgX.jpg";
-const Baby = "/assets/BabyFace-BkWpCAVM.jpg";
-const WillaPups = "/assets/WillaPups-iSlnuyqU.jpg";
-const Hippo = "/assets/Hippo4-CdTrB_K5.jpeg";
-const Kathleen = "/assets/Kathleen1-DJSz8Hp_.jpeg";
-const Otter = "/assets/Otter2-D2ovSczm.jpeg";
-const Piggy = "/assets/Piggy5-Ix9Ns2qh.jpeg";
-const Pinky = "/assets/Pinky1-BEN0IcY4.jpeg";
-const Possum = "/assets/Possum5-FSKz9L7f.jpeg";
-const Reptar = "/assets/Reptar6-GW6htzna.jpeg";
-const Splinter = "/assets/Splinter1-CdHSleM8.jpeg";
-const Violet = "/assets/Violet4-CPsN5YVA.jpeg";
+const DNA = "/pupporama/assets/WillaDNA-D9B0LrEA.PNG";
+const WillaWoo = "/pupporama/assets/WillaFace-DZ8op2ox.jpg";
+const Preggers = "/pupporama/assets/Preggers-1_efbYgX.jpg";
+const Baby = "/pupporama/assets/BabyFace-BkWpCAVM.jpg";
+const WillaPups = "/pupporama/assets/WillaPups-iSlnuyqU.jpg";
+const Hippo = "/pupporama/assets/Hippo4-CdTrB_K5.jpeg";
+const Kathleen = "/pupporama/assets/Kathleen1-DJSz8Hp_.jpeg";
+const Otter = "/pupporama/assets/Otter2-D2ovSczm.jpeg";
+const Piggy = "/pupporama/assets/Piggy5-Ix9Ns2qh.jpeg";
+const Pinky = "/pupporama/assets/Pinky1-BEN0IcY4.jpeg";
+const Possum = "/pupporama/assets/Possum5-FSKz9L7f.jpeg";
+const Reptar = "/pupporama/assets/Reptar6-GW6htzna.jpeg";
+const Splinter = "/pupporama/assets/Splinter1-CdHSleM8.jpeg";
+const Violet = "/pupporama/assets/Violet4-CPsN5YVA.jpeg";
 function Welcome() {
   return /* @__PURE__ */ jsx("main", { className: "flex items-center justify-center pt-16 pb-4", children: /* @__PURE__ */ jsxs("div", { className: "flex-1 flex flex-col items-center gap-12 min-h-0", children: [
     /* @__PURE__ */ jsxs("header", { className: "flex flex-col items-center gap-9", children: [
@@ -273,7 +273,7 @@ const route1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProper
   default: home,
   meta
 }, Symbol.toStringTag, { value: "Module" }));
-const serverManifest = { "entry": { "module": "/assets/entry.client-tKC4XhlV.js", "imports": ["/assets/chunk-UH6JLGW7-BnjEZNfp.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": true, "module": "/assets/root-Bn4kYpP7.js", "imports": ["/assets/chunk-UH6JLGW7-BnjEZNfp.js"], "css": ["/assets/root-DFGUY3tw.css"], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/home": { "id": "routes/home", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/home-Dz80w0Bq.js", "imports": ["/assets/chunk-UH6JLGW7-BnjEZNfp.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 } }, "url": "/assets/manifest-f3d7fc6a.js", "version": "f3d7fc6a", "sri": void 0 };
+const serverManifest = { "entry": { "module": "/pupporamaassets/entry.client-tKC4XhlV.js", "imports": ["/pupporamaassets/chunk-UH6JLGW7-BnjEZNfp.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": true, "module": "/pupporamaassets/root-Bn4kYpP7.js", "imports": ["/pupporamaassets/chunk-UH6JLGW7-BnjEZNfp.js"], "css": ["/pupporamaassets/root-DFGUY3tw.css"], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/home": { "id": "routes/home", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/pupporamaassets/home-DInhk6Wq.js", "imports": ["/pupporamaassets/chunk-UH6JLGW7-BnjEZNfp.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 } }, "url": "/pupporamaassets/manifest-54fb6dc6.js", "version": "54fb6dc6", "sri": void 0 };
 const assetsBuildDirectory = "build/client";
 const basename = "/";
 const future = { "unstable_middleware": false, "unstable_optimizeDeps": false, "unstable_splitRouteModules": false, "unstable_subResourceIntegrity": false, "unstable_viteEnvironmentApi": false };
@@ -281,7 +281,7 @@ const ssr = true;
 const isSpaMode = false;
 const prerender = [];
 const routeDiscovery = { "mode": "lazy", "manifestPath": "/__manifest" };
-const publicPath = "/";
+const publicPath = "/pupporama";
 const entry = { module: entryServer };
 const routes = {
   "root": {
